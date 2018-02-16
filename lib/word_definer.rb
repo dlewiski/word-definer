919e14a2e715da850889bffa975367d62c039b45
David Lewis <davidlewisp@gmail.com>
2018-02-16 09:23:41 -0800

    write first spec to return a word

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,3 +1,9 @@
 class Word
+  attr_accessor :title, :definition
+  attr_reader :parts_of_speech
 
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @definition = attributes.fetch(:definitions)
+    @parts_of_speech = attributes.fetch(:parts_of_speech)
 end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,39 +1,12 @@
 require 'rspec'
-require 'hangman'
+require 'word_definer'
 require 'pry'
 # require 'test'
 
-describe('Hangman') do
+describe('Word') do
   describe('#initialize') do
-    it('generate a word from a preset list') do
-      game = Hangman.new
-      expect(game.word_list.include?(game.word)).to(eq(true))
-      expect(game.word_list.include?("yodel")).to(eq(false))
+    it('generate a word') do
+      new_word = Word.new({:title => "happy"})
+      expect(new_word.title).to(eq("potato"))
     end
-  end
-  describe('#has_letter?') do
-    it('returns true if word contains letter, else false') do
-      game = Hangman.new
-      game.input_word("ruby")
-      expect(game.has_letter?("r")).to(eq(true))
-      expect(game.has_letter?("x")).to(eq(false))
-    end
-  end
-  describe('#update') do
-    it('inserts guessed letter into the placeholder array') do
-      game = Hangman.new
-      game.input_word("ruby")
-      # binding.pry
-      game.update('r')
-      # binding.pry
-      expect(game.placeholders).to(eq(["r", "_", "_", "_"]))
-    end
-
-    it('appends guessed letter into guessed letters array') do
-      game = Hangman.new
-      game.input_word("ruby")
-      game.update('r')
-      expect(game.guessed_letters).to(eq(["r"]))
-    end
-  end
 end
