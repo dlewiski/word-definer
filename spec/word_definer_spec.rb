1a4517b3780c2e70bbbce1134c9c8f1701992247
David Lewis <davidlewisp@gmail.com>
2018-02-25 10:47:21 -0800

    write and pass third spec to add multiple definitions

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -21,27 +21,14 @@ describe('Word') do
     end
   end
 
-  # it('returns a definition from a word') do
-  #   first_word = Word.new
-  #   first_word.add_word("happy")
-  #   first_word.add_definition("makes you feel good")
-  #   expect(first_word.define).to(eq(["makes you feel good"]))
-  # end
-  #
-  # it('adds another word to the list') do
-  #   first_word = Word.new
-  #   first_word.add_word("happy")
-  #   second_word = Word.new
-  #   second_word.add_word("sad")
-  #   expect(first_word.show_word).to(eq("happy"))
-  #   expect(second_word.show_word).to(eq("sad"))
-  # end
-  #
-  # it('adds another definition to a word') do
-  #   first_word = Word.new
-  #   first_word.add_word("happy")
-  #   first_word.add_definition("makes you feel good")
-  #   first_word.add_definition("is a type of feeling")
-  #   expect(first_word.define).to(eq(["makes you feel good", "is a type of feeling"]))
-  # end
+  describe('#add_definition') do
+    it('adds another definition to a word') do
+      word1 = Word.new("happy")
+      word1.add_definition("makes you feel good")
+      word1.add_definition("a feeling")
+      word1.save_word
+      expect(word1.definitions()).to(eq(["makes you feel good", "a feeling"]))
+    end
+  end
+
 end
