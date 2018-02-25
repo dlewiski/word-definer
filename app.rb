b6ab436e7b2db7ceebd3e24069a94560786bb598
David Lewis <davidlewisp@gmail.com>
2018-02-25 11:22:13 -0800

    add form to take in definition at home page, begin routing to show definitions page

--- app.rb
+++ app.rb
@@ -5,19 +5,14 @@ also_reload('lib/**/*.rb')
 require './lib/word_definer'
 
 get('/') do
-  word1 = Word.new("happy")
-  word2 = Word.new("cat")
-  word3 = Word.new("dog")
-  word1.save_word
-  word2.save_word
-  word3.save_word
   @words = Word.all()
   erb(:word_list)
 end
 
 post('/') do
-  @first_words = Word.new
-  word = params["word"]
-  @first_words.add_word(word)
+  new_word = Word.new(params["word"])
+  new_word.add_definition(params["definition"])
+  new_word.save_word
+  @words = Word.all()
   erb(:word_list)
 end
